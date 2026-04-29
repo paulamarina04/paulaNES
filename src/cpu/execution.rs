111692c3888a898060b53bc80a25ab6b7f96d1b4
Paula Gómez Vázquez <p.gomezvazquez@student.utwente.nl>
2026-04-29 12:27:33 +0200

    added NOP instruction

--- src/cpu/execution.rs
+++ src/cpu/execution.rs
@@ -1,3 +1,7 @@
 use super::instruction_set::Instruction;
 
-fn execute_instruction(instruction : Instruction) {}
+fn execute_instruction(instruction : Instruction) {
+    match(instruction) {
+        Instruction::NOP => {}
+    }
+}
